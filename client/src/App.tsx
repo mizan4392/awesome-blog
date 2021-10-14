814837690d2f849f570efc3e04a09a418e22814e
mizan <dev.mizan4392@gmail.com>
2021-10-14 23:37:35 +0600

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TopBar from "./components/topBar/topBar";
+
+function App() {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/topBar/topBar.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+import "./topBar.css";
+interface Props {}
+
+export default function TopBar({}: Props): ReactElement {
+  return <div className="top-bar">TopBar</div>;
+}
